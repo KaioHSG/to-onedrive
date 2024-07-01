5a0af0df84777389a1cf63236731bac57773bf0e
Kaio HSG <kaio12hsg@gmail.com>
2024-07-01 12:16:09 -0300

    Update

---
+++ Force-Synk/Import-Auto-Force-OneDrive-Synk.bat
@@ -0,0 +1,16 @@
+@echo off
+title Auto Force OneDrive Synk
+whoami /groups | find "S-1-16-12288" > nul
+if %errorLevel% neq 0 (
+   echo Accept Administrator Privileges to continue.
+   echo createObject("shell.application"^).shellExecute "%~s0", "", "", "runas", 1 > "%temp%\UAC_%~n0.vbs"
+   "%temp%\UAC_%~n0.vbs"
+   del "%temp%\UAC_%~n0.vbs" /q
+   exit
+)
+pushd "%~dp0"
+schtasks /create /tn "Auto Force OneDrive Synk" /xml "Auto-Force-OneDrive-Synk.xml" /f
+echo.
+echo Finish.
+pause > nul
+exit
